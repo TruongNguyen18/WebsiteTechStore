317837c35ae9b68081692fbacd86b7a64355b9f6
TruongNguyen18 <tieucan1310@gmail.com>
2025-10-12 00:12:43 +0700

    Add project files.

---
+++ WebsiteTechStore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebsiteTechStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
