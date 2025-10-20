93e69fde39e8d9815b6c66ab3a87eb26db4371fb
Dra-Dra04 <long149357@gmail.com>
2025-10-20 16:17:12 +0700

    Update Admin/Category,Brand,Product

--- WebsiteTechStore/Areas/Admin/Controllers/ProductController.cs
+++ WebsiteTechStore/Areas/Admin/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using WebsiteTechStore.Models;
@@ -77,6 +78,88 @@ namespace WebsiteTechStore.Areas.Admin.Controllers
                 return BadRequest(errorMessage);
             }
             return View(product);
+        }
+        [HttpGet]
+        public async Task<IActionResult> Edit(int Id)
+        {
+            ProductModel product = await _dataContext.Products.FindAsync(Id);
+            ViewBag.Categories = new SelectList(_dataContext.Categories, "Id", "Name", product.CategoryId);
+            ViewBag.Brands = new SelectList(_dataContext.Brands, "Id", "Name", product.BrandId);
+
+            return View(product);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public async Task<IActionResult> Edit(ProductModel product)
+        {
+            var existed_product = _dataContext.Products.Find(product.Id);
+            ViewBag.Categories = new SelectList(_dataContext.Categories, "Id", "Name", product.CategoryId);
+            ViewBag.Brands = new SelectList(_dataContext.Brands, "Id", "Name", product.BrandId);
+
+            if (ModelState.IsValid)
+            {
+                product.Slug = product.Name.Replace(" ", "-");
+
+                if (product.ImageUpload != null)
+                {
+                    string uploadsDir = Path.Combine(_webHostEnvironment.WebRootPath, "Images");
+                    string imageName = Guid.NewGuid().ToString() + "_" + product.ImageUpload.FileName;
+                    string filePath = Path.Combine(uploadsDir, imageName);
+
+                    FileStream fs = new FileStream(filePath, FileMode.Create);
+                    await product.ImageUpload.CopyToAsync(fs);
+                    fs.Close();
+                    existed_product.Image = imageName;
+                }
+
+                existed_product.Name = product.Name;
+                existed_product.Description = product.Description;
+                existed_product.Slug = product.Slug;
+                existed_product.Price = product.Price;
+                existed_product.CategoryId = product.CategoryId;
+                existed_product.BrandId = product.BrandId;
+                _dataContext.Update(existed_product);
+                await _dataContext.SaveChangesAsync();
+                TempData["success"] = "Cập nhật sản phẩm thành công";
+                return RedirectToAction("Index");
+
+            }
+            else
+            {
+                TempData["error"] = "Model có một vài thứ đang lỗi";
+                List<string> errors = new List<string>();
+                foreach (var value in ModelState.Values)
+                {
+                    foreach (var error in value.Errors)
+                    {
+                        errors.Add(error.ErrorMessage);
+                    }
+                }
+                string errorMessage = string.Join("\n", errors);
+                return BadRequest(errorMessage);
+            }
+            return View(product);
+        }
+
+
+        public async Task<IActionResult> Delete(int Id)
+        {
+            ProductModel product = await _dataContext.Products.FindAsync(Id);
+
+            if (!string.Equals(product.Image, ""))
+            {
+                string uploadsDir = Path.Combine(_webHostEnvironment.WebRootPath, "Images");
+                string oldfilePath = Path.Combine(uploadsDir, product.Image);
+                if (System.IO.File.Exists(oldfilePath))
+                {
+                    System.IO.File.Delete(oldfilePath);
+                }
+            }
+            _dataContext.Products.Remove(product);
+            await _dataContext.SaveChangesAsync();
+            TempData["error"] = "sản phẩm đã được xóa thành công";
+            return RedirectToAction("Index");
         }
     }
 }
